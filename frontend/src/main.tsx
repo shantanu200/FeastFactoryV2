9f8c4e73b20cdd67773a138a83307bd6afe4b69a
shantanu200 <shantanubhusari16@gmail.com>
2023-12-28 00:19:01 +0530

    Prodduction Build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
-axios.defaults.baseURL = 'http://localhost:8080/api/';
+axios.defaults.baseURL = 'http://13.201.84.100:8080/api';
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
